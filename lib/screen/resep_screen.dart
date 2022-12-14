bec0cac11e21bfacdf02ef0248eb1765c41e5257
safrurozaky <safrurozaky17@gmail.com>
2022-12-14 10:17:59 +0700

    screen 2

--- lib/screen/resep_screen.dart
+++ lib/screen/resep_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:resep_masakan/screen/detail_screen.dart';
+
+import '../cubit/cubit/resep_cubit.dart';
 
 class ResepScreen extends StatefulWidget {
   ResepScreen({super.key});
@@ -50,7 +54,80 @@ class _ResepScreenState extends State<ResepScreen> {
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                   child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
+                      height: 70),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return DetailScreen();
+                            }));
+                          },
+                          child: const Text(
+                            'Resep Tumis Kacang Panjang Jamur, Untuk Sahur',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.5,
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '4 Porsi',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                            Text(
+                              '30Mnt',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 211, 200, 232),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: .2,
+                  blurRadius: 10,
+                  offset: Offset(0, 10), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Image.network(
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
                       height: 70),
                 ),
                 Expanded(
@@ -115,7 +192,7 @@ class _ResepScreenState extends State<ResepScreen> {
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                   child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
                       height: 70),
                 ),
                 Expanded(
@@ -180,7 +257,7 @@ class _ResepScreenState extends State<ResepScreen> {
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                   child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
                       height: 70),
                 ),
                 Expanded(
@@ -245,7 +322,7 @@ class _ResepScreenState extends State<ResepScreen> {
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                   child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
                       height: 70),
                 ),
                 Expanded(
@@ -310,72 +387,7 @@ class _ResepScreenState extends State<ResepScreen> {
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                   child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
-                      height: 70),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Resep Tumis Kacang Panjang Jamur, Untuk Sahur',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15.5,
-                          ),
-                        ),
-                        Spacer(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '4 Porsi',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 13,
-                              ),
-                            ),
-                            Text(
-                              '30Mnt',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 13,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: 100,
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 211, 200, 232),
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  spreadRadius: .2,
-                  blurRadius: 10,
-                  offset: Offset(0, 10), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 8.0),
-                  child: Image.network(
-                      'https://www.masakapahariini.com/wp-content/uploads/2019/02/coto-makassar-1-400x240.jpg',
+                      'https://www.bango.co.id/gfx/recipes/temp_thumb-1573121581.jpg',
                       height: 70),
                 ),
                 Expanded(
